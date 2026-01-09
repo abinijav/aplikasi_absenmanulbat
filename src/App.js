4bbf6b67dc4bda6b98d69679f2471729f7d2cd98
Abi Nijav <mrzaenal76@gmail.com>
2026-01-09 21:05:49 +0700

    update judul dasbor guru

--- src/App.js
+++ src/App.js
@@ -372,7 +372,7 @@ const LoginScreen = ({ onLogin, setNotification }) => {
     );
 };
 
-// --- Komponen Dasbor Siswa ---
+// --- Komponen {user.role === 'teacher' ? "Dasbor Guru" : "Dasbor Siswa"} ---
 const StudentDashboard = ({ user, supabase, settings, setNotification, isHoliday, holidayName, customTitle }) => {
     const [showCamera, setShowCamera] = useState(false);
     const [attendanceType, setAttendanceType] = useState('');
@@ -561,9 +561,7 @@ const StudentDashboard = ({ user, supabase, settings, setNotification, isHoliday
             {showCamera && <CameraModal onCapture={handleSelfieCapture} onCancel={() => setShowCamera(false)} setNotification={setNotification} />}
             
             <div className="bg-white p-6 rounded-xl shadow-lg">
-              <h2 className="text-2xl font-bold text-gray-800 mb-4">
-    {user.role === 'teacher' ? "Dasbor Guru" : "Dasbor Siswa"}
-</h2>
+               <h2 className="text-2xl font-bold text-gray-800 mb-4">{user.role === 'teacher' ? "Dasbor Guru" : "Dasbor Siswa"}</h2>
                 
                 {isHoliday && (
                     <div className="text-center p-4 mb-4 bg-yellow-100 text-yellow-800 rounded-lg font-semibold border border-yellow-300">
